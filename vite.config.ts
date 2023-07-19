cf977b8dd868f13ffe7956861214b3fd1abcc58b
Caro Bottino <carobottino888@gmail.com>
2023-07-19 16:18:22 -0300

    bootstrap y github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: process.env.VITE_BASE_URL,
+  server: {
+    port: 8080
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
