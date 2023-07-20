d3a55e57be8104aad425402a26e9b5f9841a870e
Caro Bottino <carobottino888@gmail.com>
2023-07-20 20:05:02 -0300

    prueba

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
+  // history: createWebHistory(import.meta.env.VITE_BASE_URL),
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -15,7 +16,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('@/views/AboutView.vue')
     }
   ]
 })
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.VITE_BASE_URL,
+  base: '/recipes_app/', // process.env.VITE_BASE_URL,
   server: {
     port: 8080
   },
