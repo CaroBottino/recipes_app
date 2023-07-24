cb3aa0bc5f9578c306ba69647dc7314e06cf9fd1
Caro Bottino <carobottino888@gmail.com>
2023-07-24 15:13:28 -0300

    404 not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,16 @@ const router = createRouter({
       path: '/edit-recipe/:id',
       name: 'edit-recipe',
       component: () => import('@/views/EditRecipeView.vue')
+    },
+    {
+      path: '/notfound',
+      component: NotFoundView,
+      meta: { title: 'Not Found' }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="centered">
+    <img :src="robot" alt="..." class="robot" />
+    <h1>NOT FOUND</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const robot = './src/assets/img/eva2.webp'
+</script>
+
+<style scoped>
+.centered {
+  display: flex;
+  justify-content: center;
+  vertical-align: baseline;
+}
+
+.robot {
+  max-height: 16rem;
+  max-width: 16rem;
+}
+</style>
